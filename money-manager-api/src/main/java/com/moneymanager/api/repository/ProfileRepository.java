d86761297df23020a9451c8a4decb3a22511404e
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-05-16 11:06:05 +0530

    Profile enpoint completed

--- money-manager-api/src/main/java/com/moneymanager/api/repository/ProfileRepository.java
+++ money-manager-api/src/main/java/com/moneymanager/api/repository/ProfileRepository.java
@@ -10,4 +10,6 @@ public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
     //select * from tbl_profiles where email = ?
     Optional<ProfileEntity> findByEmail(String email);
 
+    //select * from tbl_profiles where activation_token = ?
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
 }
